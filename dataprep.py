aaf55644f64be53e48e4de754a5373b1a768e7f6
Duc An <antp9254@gmail.com>
2023-10-14 21:17:01 +0700

    update

--- dataprep.py
+++ dataprep.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*-
+#-*- coding: utf-8 -*-
 # The script downloads the VoxCeleb datasets and converts all files to WAV.
 # Requirement: ffmpeg and wget running on a Linux system.
 
@@ -15,11 +15,10 @@ from zipfile import ZipFile
 from tqdm import tqdm
 from scipy.io import wavfile
 
-# ========== ===========
-# Parse input arguments
-# ========== ===========
-parser = argparse.ArgumentParser(description="VoxCeleb downloader")
-
+## ========== ===========
+## Parse input arguments
+## ========== ===========
+parser = argparse.ArgumentParser(description = "VoxCeleb downloader");
 
 parser.add_argument('--save_path',     type=str, default="data", help='Target directory');
 parser.add_argument('--user',         type=str, default="user", help='Username');
@@ -30,13 +29,11 @@ parser.add_argument('--extract',  dest='extract',  action='store_true', help='En
 parser.add_argument('--convert',  dest='convert',  action='store_true', help='Enable convert')
 parser.add_argument('--augment',  dest='augment',  action='store_true', help='Download and extract augmentation files')
 
-args = parser.parse_args()
+args = parser.parse_args();
 
-# ========== ===========
-# MD5SUM
-# ========== ===========
-
-
+## ========== ===========
+## MD5SUM
+## ========== ===========
 def md5(fname):
 
     hash_md5 = hashlib.md5()
@@ -45,72 +42,61 @@ def md5(fname):
             hash_md5.update(chunk)
     return hash_md5.hexdigest()
 
-# ========== ===========
-# Download with wget
-# ========== ===========
-
-
+## ========== ===========
+## Download with wget
+## ========== ===========
 def download(args, lines):
 
     for line in lines:
-        url = line.split()[0]
-        md5gt = line.split()[1]
+        url     = line.split()[0]
+        md5gt     = line.split()[1]
         outfile = url.split('/')[-1]
 
-        # Download files
-        out = subprocess.call('wget %s --user %s --password %s -O %s/%s' %
-                              (url, args.user, args.password, args.save_path, outfile), shell=True)
+        ## Download files
+        out     = subprocess.call('wget %s --user %s --password %s -O %s/%s'%(url,args.user,args.password,args.save_path,outfile), shell=True)
         if out != 0:
-            raise ValueError(
-                'Download failed %s. If download fails repeatedly, use alternate URL on the VoxCeleb website.' % url)
+            raise ValueError('Download failed %s. If download fails repeatedly, use alternate URL on the VoxCeleb website.'%url)
 
-        # Check MD5
-        md5ck = md5('%s/%s' % (args.save_path, outfile))
+        ## Check MD5
+        md5ck     = md5('%s/%s'%(args.save_path,outfile))
         if md5ck == md5gt:
-            print('Checksum successful %s.' % outfile)
+            print('Checksum successful %s.'%outfile)
         else:
-            raise Warning('Checksum failed %s.' % outfile)
+            raise Warning('Checksum failed %s.'%outfile)
 
-# ========== ===========
-# Concatenate file parts
-# ========== ===========
-
-
-def concatenate(args, lines):
+## ========== ===========
+## Concatenate file parts
+## ========== ===========
+def concatenate(args,lines):
 
     for line in lines:
-        infile = line.split()[0]
-        outfile = line.split()[1]
-        md5gt = line.split()[2]
+        infile     = line.split()[0]
+        outfile    = line.split()[1]
+        md5gt     = line.split()[2]
 
-        # Concatenate files
-        out = subprocess.call(
-            'cat %s/%s > %s/%s' % (args.save_path, infile, args.save_path, outfile), shell=True)
+        ## Concatenate files
+        out     = subprocess.call('cat %s/%s > %s/%s' %(args.save_path,infile,args.save_path,outfile), shell=True)
 
-        # Check MD5
-        md5ck = md5('%s/%s' % (args.save_path, outfile))
+        ## Check MD5
+        md5ck     = md5('%s/%s'%(args.save_path,outfile))
         if md5ck == md5gt:
-            print('Checksum successful %s.' % outfile)
+            print('Checksum successful %s.'%outfile)
         else:
-            raise Warning('Checksum failed %s.' % outfile)
+            raise Warning('Checksum failed %s.'%outfile)
 
-        out = subprocess.call('rm %s/%s' %
-                              (args.save_path, infile), shell=True)
+        out     = subprocess.call('rm %s/%s' %(args.save_path,infile), shell=True)
 
-# ========== ===========
-# Extract zip files
-# ========== ===========
-
-
+## ========== ===========
+## Extract zip files
+## ========== ===========
 def is_within_directory(directory, target):
-
+    
     abs_directory = os.path.abspath(directory)
     abs_target = os.path.abspath(target)
 
     prefix = os.path.commonprefix([abs_directory, abs_target])
-
+    
     return prefix == abs_directory
-
 
 def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
 
@@ -120,10 +106,9 @@ def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
             raise Exception("Attempted Path Traversal in Tar File")
         tar.extractall(path, members, numeric_owner=numeric_owner)
 
-
 def full_extract(args, fname):
 
-    print('Extracting %s' % fname)
+    print('Extracting %s'%fname)
     if fname.endswith(".tar.gz"):
         with tarfile.open(fname, "r:gz") as tar:
             safe_extract(tar, args.save_path)
@@ -132,101 +117,90 @@ def full_extract(args, fname):
             zf.extractall(args.save_path)
 
 
-# ========== ===========
-# Partially extract zip files
-# ========== ===========
+## ========== ===========
+## Partially extract zip files
+## ========== ===========
 def part_extract(args, fname, target):
 
-    print('Extracting %s' % fname)
+    print('Extracting %s'%fname)
     with ZipFile(fname, 'r') as zf:
         for infile in zf.namelist():
             if any([infile.startswith(x) for x in target]):
-                zf.extract(infile, args.save_path)
+                zf.extract(infile,args.save_path)
             # pdb.set_trace()
             # zf.extractall(args.save_path)
 
-# ========== ===========
-# Convert
-# ========== ===========
-
-
+## ========== ===========
+## Convert
+## ========== ===========
 def convert(args):
 
-    files = glob.glob('%s/voxceleb2/*/*/*.m4a' % args.save_path)
+    files     = glob.glob('%s/voxceleb2/*/*/*.m4a'%args.save_path)
     files.sort()
 
     print('Converting files from AAC to WAV')
     for fname in tqdm(files):
-        outfile = fname.replace('.m4a', '.wav')
-        out = subprocess.call(
-            'ffmpeg -y -i %s -ac 1 -vn -acodec pcm_s16le -ar 16000 %s >/dev/null 2>/dev/null' % (fname, outfile), shell=True)
+        outfile = fname.replace('.m4a','.wav')
+        out = subprocess.call('ffmpeg -y -i %s -ac 1 -vn -acodec pcm_s16le -ar 16000 %s >/dev/null 2>/dev/null' %(fname,outfile), shell=True)
         if out != 0:
-            raise ValueError('Conversion failed %s.' % fname)
+            raise ValueError('Conversion failed %s.'%fname)
 
-# ========== ===========
-# Split MUSAN for faster random access
-# ========== ===========
-
-
+## ========== ===========
+## Split MUSAN for faster random access
+## ========== ===========
 def split_musan(args):
 
-    files = glob.glob('%s/musan/*/*/*.wav' % args.save_path)
+    files = glob.glob('%s/musan/*/*/*.wav'%args.save_path)
 
     audlen = 16000*5
     audstr = 16000*3
 
-    for idx, file in enumerate(files):
-        fs, aud = wavfile.read(file)
-        writedir = os.path.splitext(
-            file.replace('/musan/', '/musan_split/'))[0]
+    for idx,file in enumerate(files):
+        fs,aud = wavfile.read(file)
+        writedir = os.path.splitext(file.replace('/musan/','/musan_split/'))[0]
         os.makedirs(writedir)
-        for st in range(0, len(aud)-audlen, audstr):
-            wavfile.write(writedir+'/%05d.wav' %
-                          (st/fs), fs, aud[st:st+audlen])
+        for st in range(0,len(aud)-audlen,audstr):
+            wavfile.write(writedir+'/%05d.wav'%(st/fs),fs,aud[st:st+audlen])
 
-        print(idx, file)
+        print(idx,file)
 
-
-# ========== ===========
-# Main script
-# ========== ===========
+## ========== ===========
+## Main script
+## ========== ===========
 if __name__ == "__main__":
 
     if not os.path.exists(args.save_path):
         raise ValueError('Target directory does not exist.')
 
-    f = open('lists/fileparts.txt', 'r')
+    f = open('lists/fileparts.txt','r')
     fileparts = f.readlines()
     f.close()
 
-    f = open('lists/files.txt', 'r')
+    f = open('lists/files.txt','r')
     files = f.readlines()
     f.close()
 
-    f = open('lists/augment.txt', 'r')
+    f = open('lists/augment.txt','r')
     augfiles = f.readlines()
     f.close()
 
     if args.augment:
-        download(args, augfiles)
-        part_extract(args, os.path.join(args.save_path, 'rirs_noises.zip'), [
-                     'RIRS_NOISES/simulated_rirs/mediumroom', 'RIRS_NOISES/simulated_rirs/smallroom'])
-        full_extract(args, os.path.join(args.save_path, 'musan.tar.gz'))
+        download(args,augfiles)
+        part_extract(args,os.path.join(args.save_path,'rirs_noises.zip'),['RIRS_NOISES/simulated_rirs/mediumroom','RIRS_NOISES/simulated_rirs/smallroom'])
+        full_extract(args,os.path.join(args.save_path,'musan.tar.gz'))
         split_musan(args)
 
     if args.download:
-        download(args, fileparts)
+        download(args,fileparts)
 
     if args.extract:
         concatenate(args, files)
         for file in files:
-            full_extract(args, os.path.join(args.save_path, file.split()[1]))
-        out = subprocess.call('mv %s/dev/aac/* %s/aac/ && rm -r %s/dev' %
-                              (args.save_path, args.save_path, args.save_path), shell=True)
-        out = subprocess.call('mv %s/wav %s/voxceleb1' %
-                              (args.save_path, args.save_path), shell=True)
-        out = subprocess.call('mv %s/aac %s/voxceleb2' %
-                              (args.save_path, args.save_path), shell=True)
+            full_extract(args,os.path.join(args.save_path,file.split()[1]))
+        out = subprocess.call('mv %s/dev/aac/* %s/aac/ && rm -r %s/dev' %(args.save_path,args.save_path,args.save_path), shell=True)
+        out = subprocess.call('mv %s/wav %s/voxceleb1' %(args.save_path,args.save_path), shell=True)
+        out = subprocess.call('mv %s/aac %s/voxceleb2' %(args.save_path,args.save_path), shell=True)
 
     if args.convert:
         convert(args)
+        
--- trainSpeakerNet.py
+++ trainSpeakerNet.py
@@ -35,7 +35,7 @@ parser.add_argument('--seed',           type=int,   default=10,     help='Seed f
 
 ## Training details
 parser.add_argument('--test_interval',  type=int,   default=10,     help='Test and save every [test_interval] epochs')
-parser.add_argument('--max_epoch',      type=int,   default=100,    help='Maximum number of epochs')
+parser.add_argument('--max_epoch',      type=int,   default=500,    help='Maximum number of epochs')
 parser.add_argument('--trainfunc',      type=str,   default="",     help='Loss function')
 
 ## Optimizer
@@ -60,21 +60,15 @@ parser.add_argument('--dcf_c_fa',       type=float, default=1,      help='Cost o
 
 ## Load and save
 parser.add_argument('--initial_model',  type=str,   default="",     help='Initial model weights')
-parser.add_argument('--save_path',      type=str,   default="exps/RawNet3_AAM", help='Path for model and logs')
+parser.add_argument('--save_path',      type=str,   default="exps/exp1", help='Path for model and logs')
 
 ## Training and test data
-# parser.add_argument('--train_list',     type=str,   default="data/train_list.txt",  help='Train list')
-# parser.add_argument('--test_list',      type=str,   default="data/test_list.txt",   help='Evaluation list')
-# parser.add_argument('--train_path',     type=str,   default="data/voxceleb2", help='Absolute path to the train set')
-# parser.add_argument('--test_path',      type=str,   default="data/voxceleb1", help='Absolute path to the test set')
-# parser.add_argument('--musan_path',     type=str,   default="data/musan_split", help='Absolute path to the test set')
-# parser.add_argument('--rir_path',       type=str,   default="data/RIRS_NOISES/simulated_rirs", help='Absolute path to the test set')
-
-parser.add_argument('--train_list', type=str,   default="/kaggle/working/RawNet3-VLSP2023/train_list.txt", help='The path of the training list')
-parser.add_argument('--train_path', type=str,   default="/kaggle/input/vlsp-sv-2023/vietnam_celeb", help='The path of the training data')
-parser.add_argument('--test_list',  type=str,   default="/kaggle/working/RawNet3-VLSP2023/eval_list.txt", help='The path of the evaluation list')
-parser.add_argument('--test_path',  type=str,   default="/kaggle/input/vlsp-sv-2023/spoofing_data", help='The path of the evaluation data')
-# parser.add_argument('--initial_model',  type=str, default="", help='Path of the initial_model')
+parser.add_argument('--train_list',     type=str,   default="data/train_list.txt",  help='Train list')
+parser.add_argument('--test_list',      type=str,   default="data/test_list.txt",   help='Evaluation list')
+parser.add_argument('--train_path',     type=str,   default="data/voxceleb2", help='Absolute path to the train set')
+parser.add_argument('--test_path',      type=str,   default="data/voxceleb1", help='Absolute path to the test set')
+parser.add_argument('--musan_path',     type=str,   default="data/musan_split", help='Absolute path to the test set')
+parser.add_argument('--rir_path',       type=str,   default="data/RIRS_NOISES/simulated_rirs", help='Absolute path to the test set')
 
 ## Model definition
 parser.add_argument('--n_mels',         type=int,   default=40,     help='Number of mel filterbanks')
